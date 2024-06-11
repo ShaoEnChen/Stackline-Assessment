e2e1585b79d5aff46aacd770b5d74079606954c4
ShaoEnChen <seanchen47@gmail.com>
2024-06-11 00:35:48 -0700

    Fix compile error: Stubbing API call to retrieve product data via MockAPI

--- app/ui/product/details.tsx
+++ app/ui/product/details.tsx
@@ -2,7 +2,9 @@ import { ProductData } from "@/app/lib/definitions";
 import ProductOverview from "./overview";
 import ProductTags from "./tags";
 
-export default function ProductDetails({ data }: ProductData) {
+export default function ProductDetails({ data }: {
+  data: ProductData;
+}) {
   return (
     <div className="flex flex-col w-1/5 h-full mr-6 py-6 bg-white" aria-label="Product Details">
       <ProductOverview
--- app/ui/product/sales.tsx
+++ app/ui/product/sales.tsx
@@ -1,7 +1,10 @@
+import { ProductData } from "@/app/lib/definitions";
 import SalesGraph from "@/app/ui/sales/graph";
 import SalesTable from "@/app/ui/sales/table";
 
-export default function ProductSales({ tags }) {
+export default function ProductSales({ data }: {
+  data: ProductData;
+}) {
   return (
     <div className="flex flex-col w-full h-screen" aria-label="Product Sales">
       <SalesGraph />
